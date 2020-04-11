a959bf52b625d1c5ebe44a9177e6a30997e8d232
Kuncheria Kuruvilla <kuncheria94@gmail.com>
2020-04-11 16:53:29 +0900

    Added .prod key
    Added Prod key

---
+++ src/config/prod.js
@@ -0,0 +1,5 @@
+export const FirebaseConfig = {
+  apiKey: 'AIzaSyD48Q5VBMkNeML0H7s5YBoFv4UvoV9J6rI',
+  authDomain: 'chainreaction-57435.firebaseapp.com',
+  databaseURL: 'chainreaction-57435.firebaseio.com',
+};
